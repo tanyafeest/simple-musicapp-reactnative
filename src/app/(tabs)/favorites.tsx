5448c32a022db8f064c23ac21be6b3376ed321d6
tanyafeest <blockchain.engineer215@gmail.com>
2023-09-10 06:26:28 +0530

    Search and favorites screen

--- src/app/(tabs)/favorites.tsx
+++ src/app/(tabs)/favorites.tsx
@@ -1,31 +1,14 @@
-import { StyleSheet } from 'react-native';
+import { FlatList } from 'react-native';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { tracks } from '../../../assets/data/track';
+import TrackListItem from '../../components/TrackListItem';
 
-export default function TabTwoScreen() {
+export default function FavoritesScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
+    <FlatList
+      data={tracks}
+      renderItem={({ item }) => <TrackListItem track={item} />}
+      showsVerticalScrollIndicator={false}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
--- src/app/(tabs)/search.tsx
+++ src/app/(tabs)/search.tsx
@@ -1,31 +1,52 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { tracks } from '../../../assets/data/track';
+import TrackListItem from '../../components/TrackListItem';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FontAwesome } from '@expo/vector-icons';
+import { useState } from 'react';
+export default function SearchScreen() {
+  const [search, setSearch] = useState('');
 
-export default function TabTwoScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.header}>
+        {/* Header View */}
+        <FontAwesome name="search" size={16} color="gray" />
+        <TextInput
+          value={search}
+          onChangeText={setSearch}
+          style={styles.input}
+          placeholder="What do you want to listen to?"
+          placeholderTextColor={'gray'}
+        />
+
+        <Text onPress={() => setSearch('')} style={{ color: 'gray' }}>
+          Cancel
+        </Text>
+      </View>
+
+      <FlatList
+        data={tracks}
+        renderItem={({ item }) => <TrackListItem track={item} />}
+        showsVerticalScrollIndicator={false}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  header: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  input: {
+    flex: 1,
+    backgroundColor: '#121314',
+    padding: 8,
+    marginHorizontal: 10,
+    borderRadius: 5,
+    color: '#fff',
   },
 });
